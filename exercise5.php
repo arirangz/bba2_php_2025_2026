2eda067f0b289059a19f5c8c47613c02d9b58668
Anthony L <laplane_anthony@yahoo.fr>
2025-11-14 13:53:02 +0100

    exercise5

---
+++ exercise5.php
@@ -0,0 +1,13 @@
+<?php
+
+$games = [
+    ["name" => "GTA 6", "editor" => "Rockstar", "year" => 2026],
+    ["name" => "League Of Legends", "editor" => "Riot", "year" => 2009],
+    ["name" => "Hollow Kight: Silksong", "editor" => "Team Cherry", "year" => 2025],
+    ["name" => "Hades II", "editor" => "Supergiant Game", "year" => 2025],
+    ["name" => "Minecraft", "editor" => "Mojang", "year" => 2011],
+    ["name" => "Fornite", "editor" => "Epic Games", "year" => 2017],
+];
+
+
+?>
