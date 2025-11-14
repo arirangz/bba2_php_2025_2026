37ab423ed1bdbeeefdfcca1949b41deec0f22f4e
Anthony L <laplane_anthony@yahoo.fr>
2025-11-14 12:00:46 +0100

    exercise 3b

---
+++ exercise3b.php
@@ -0,0 +1,22 @@
+<?php 
+$gameName = "GTA 6";
+$new = true;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?= $gameName ?>
+        <?php
+            if ($new) {
+                echo "<strong>NEW!</strong>";
+            }
+        ?>
+    </p>
+</body>
+</html>
