60952347968b9667cafc830c592bedc2c640418f
Anthony L <laplane_anthony@yahoo.fr>
2025-11-14 11:10:52 +0100

    exercise 2

---
+++ exercise2.php
@@ -0,0 +1,18 @@
+<?php
+$productName = "Dell Laptop";
+$description = "Some description";
+$price = 15000;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Product name: <?= $productName ?></h1>
+    <p>Description: <?= $description ?></p>
+    <h2>Price: <?= $price ?></h2>
+</body>
+</html>
