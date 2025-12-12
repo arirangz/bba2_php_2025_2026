8ad870fda4a3a85b54a9ecff40bfb6715bb5140c
Anthony L <laplane_anthony@yahoo.fr>
2025-12-12 11:12:24 +0100

    xss

--- exemple_get.php
+++ exemple_get.php
@@ -1,9 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
+    <?php
+    if (isset($_GET["firstName"])) {
+        echo htmlspecialchars($_GET["firstName"]);
 
-if (isset($_GET["firstName"])) {
-    echo $_GET["firstName"];
-
-} else {
-    echo "first name is missing in url";
-}
+    } else {
+        echo "first name is missing in url";
+    }
+    ?>
+    
+</body>
+</html>
