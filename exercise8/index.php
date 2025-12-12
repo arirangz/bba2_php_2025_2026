74d2445ad99b6dfdee45f21fbb61ea81d6a04270
Anthony L <laplane_anthony@yahoo.fr>
2025-12-12 10:35:07 +0100

    ex8 correciton

---
+++ exercise8/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+    <?php require_once "header.php" ?>
+
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, veniam repudiandae impedit quas hic possimus veritatis ratione aspernatur, neque harum, voluptas laborum. Laborum, praesentium consectetur fuga distinctio molestiae ipsum consequatur?</p>
+
+    <?php require_once "footer.php" ?>
+
+</body>
+</html>
