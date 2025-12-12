a2ef91e53ead2e162e39dea07592a423562552b0
Anthony L <laplane_anthony@yahoo.fr>
2025-12-12 10:47:17 +0100

    mini website

---
+++ mini_website/contact.php
@@ -0,0 +1,11 @@
+<?php require_once "header.php" ?>
+
+<h1>Contact</h1>
+<form action="">
+    <p>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email">
+    </p>
+</form>
+
+<?php require_once "footer.php" ?>
---
+++ mini_website/index.php
@@ -0,0 +1,7 @@
+<?php require_once "header.php" ?>
+
+<h1>Home</h1>
+<p>Some content on homepage</p>
+
+
+<?php require_once "footer.php" ?>
