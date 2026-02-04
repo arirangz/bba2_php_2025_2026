f5c4ad583569ab2d800903067c6ceb65cba3646f
Anthony L <laplane_anthony@yahoo.fr>
2026-02-04 10:16:27 +0100

    construct

--- oop/example_car/Car.php
+++ oop/example_car/Car.php
@@ -1,11 +1,21 @@
 <?php
 class Car
 {
-    public string $brand;
-    public float $maxSpeed;
+
+    public function __construct(
+        public string $brand,
+        public float $maxSpeed
+    )
+    {
+    }
 
     public function displayBrand():void
     {
         echo "Brand: ".$this->brand;
+    }
+
+    public function getMaxSpeedWithKm():string
+    {
+        return $this->maxSpeed."km/h";
     }
 }
--- oop/example_car/index.php
+++ oop/example_car/index.php
@@ -2,13 +2,11 @@
 
 require_once "Car.php";
 
-$toyota = new Car();
-$toyota->brand = "Toyota";
-$toyota->maxSpeed = 200;
+$toyota = new Car("Toyota", 200);
 
-$peugeot = new Car();
-$peugeot->brand = "Peugeot";
-$peugeot->maxSpeed = 195;
+
+$peugeot = new Car("Peugeot", 195);
+
 
 
 ?>
@@ -22,7 +20,7 @@ $peugeot->maxSpeed = 195;
 <body>
     <h1>Example instantiation</h1>
     <h2>Car brand: <?=$toyota->brand  ?></h2>
-    <h2>Car max speed: <?=$toyota->maxSpeed  ?></h2>
+    <h2>Car max speed: <?=$toyota->getMaxSpeedWithKm()  ?></h2>
 
     <h2><?= $peugeot->displayBrand() ?></h2>
 </body>
